c79302364500cc398dd39360d652151402cfffb9
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-01-12 19:02:08 +0530

    Project setup

---
+++ src/test/java/com/swapnil/CoLab/CoLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnil.CoLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
