885201e00e1082fb0856531ea90229e2648bd0d5
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-01-13 15:02:03 +0530

    DTO for user registration

---
+++ src/main/java/com/swapnil/CoLab/dto/UserRegistrationDTO.java
@@ -0,0 +1,29 @@
+package com.swapnil.CoLab.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserRegistrationDTO {
+
+    @NotBlank(message = "Full name is required")
+    @Size(min = 2, max = 50, message = "Full name must be between 2 and 50 characters")
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @Size(max = 10, message = "A maximum of 10 skills can be added")
+    private List<String> skills;
+    private String role;
+
+}
