902dbea51251d8ebbb105720110fec7b618b717f
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-01-14 14:40:51 +0530

    Configuration updated

--- src/main/java/com/swapnil/CoLab/config/AppConfig.java
+++ src/main/java/com/swapnil/CoLab/config/AppConfig.java
@@ -5,11 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
     @Bean
@@ -19,8 +25,8 @@ public class AppConfig {
                 .sessionManagement(management->management
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(Authorize-> Authorize.requestMatchers("/api/**", "/error").permitAll()
-                        .anyRequest().authenticated());
-//                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                        .anyRequest().authenticated())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class);
 //                .cors(cors->cors.configurationSource(corsConfigurationSource()));
         return http.build();
     }
